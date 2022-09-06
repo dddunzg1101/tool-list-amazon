214c4ba4b59ea9867216e32cb6db2fba05ce13ca
hieppham1999 <hieppham1999@gmail.com>
2022-09-06 15:08:39 +0700

    fix bug

--- List_ver2.4_CA_field_oriented.py
+++ List_ver2.4_CA_field_oriented.py
@@ -24,13 +24,19 @@ from datetime import datetime
 #Setup:
 # 1. Bật command prompt -> cd /d C:\Program Files\Google\Chrome\Application
 # 2. -> chrome.exe --remote-debugging-port=8989 --user-data-dir="F:\ASM Group\Tool Python\driver\chrome.existing"
+# 2. (For dev) -> chrome.exe --remote-debugging-port=8989 --user-data-dir="D:\projects\tool-list-amazon\driver\chrome.existing"
 #chrome_options.add_argument("--incognito") mở ẩn danh
 #login account và vào url https://sellercentral.amazon.com/inventory
+
+
+# BASE_DIRECTORY = "F:\\ASM Group\\Tool Python"
+BASE_DIRECTORY = "D:\projects\\tool-list-amazon"
+CSV_FILE_NAME = "Dart_to_Dasanito2.csv"
 
 opt = webdriver.ChromeOptions()
 opt.add_experimental_option("debuggerAddress","localhost:8989") 
 MOVE_TO_NEXT_PRODUCT_STRING = "Move to the next product..."
-BASE_DIRECTORY = "F:\\ASM Group\\Tool Python"
+
 driver = webdriver.Chrome(executable_path=f'{BASE_DIRECTORY}\\driver\\chromedriver.exe',options=opt)
 countSuccess = 0
 productCount = 0
@@ -42,27 +48,29 @@ if not os.path.exists(f'{BASE_DIRECTORY}\\logs'):
     os.makedirs(f'{BASE_DIRECTORY}\\logs')
 f = open(filepath, "a+")
 
-with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
+
+with open(f'{BASE_DIRECTORY}\\{CSV_FILE_NAME}','r') as csv_file:
     csv_reader = csv.reader(csv_file)
     row_count = len(list(csv_file))
     csv_file.seek(0)
     for line in csv_reader:
         try: 
-
             shouldContinue = False
-
             productCount += 1
             productAsin = line[0]
 
             # ------------------------------ Logging function ------------------------------ #
-            def log(logContent, save = False):
+
+            # Change the printOut param to True if you want to print the full process, this will affect performance
+            def log(logContent, save = False, printOut = False):
                 now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 logString = f"{productAsin}: {now}: {logContent}"
-                print(logString)
+                if printOut:
+                    print(logString)
                 if save:
                     f.write(f'{logString}\n')
 
-            log(f'Start offering item {productCount}/{row_count}...', True)  
+            log(f'Start offering item {productCount}/{row_count}...', True, True)  
             url = f'https://sellercentral.amazon.com/abis/listing/syh?asin={productAsin}&ref_=xx_addlisting_dnav_xx#offer'
             driver.get(url)
             time.sleep(1)
@@ -71,7 +79,7 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
             try: 
                 isBrandNeedAprove = driver.find_element_by_xpath('//*[@id="marathonUI"]//span[text()="Error!"]')
                 if isBrandNeedAprove:
-                    log(f'Brand need approval. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                    log(f'Brand need approval. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                     continue
             except NoSuchElementException:
                 log('Brand does not need Approval')
@@ -79,7 +87,7 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
             # ------------------------------ ASIN Approval ------------------------------ #
             try: 
                 if driver.find_element_by_xpath('//*[@id="marathonUI"]/div/div/div/div[2]/section[2]/ul[1]').tag_name == 'ul':
-                    log(f'ASIN needs Approval. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                    log(f'ASIN needs Approval. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                     continue
             except:
                 log('ASIN does not need Approval')
@@ -96,9 +104,9 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
             try:
                 WebDriverWait(driver,2).until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'You are currently viewing a new version of Add a P')]")))
                 newLayout = True
-                log('This product use NEW layout', True)
+                log('This product use NEW layout', True, True)
             except TimeoutException:
-                log('This product use OLD layout', True)
+                log('This product use OLD layout', True, True)
                 newLayout = False
 
             # ------------------------------ All attribute mode ------------------------------ # 
@@ -111,27 +119,26 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
             # except NoSuchElementException:
             #     log(f'Cant find All Attribute element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
             #     continue   
-
+            # log('All attribute mode: OK')
 
             # ------------------------------ Advanced View Switch ------------------------------ # 
             try:
-                advancedViewSwitch = driver.find_element_by_xpath(".//*[@id='advanced-view-switch']")
+                advancedViewSwitch = WebDriverWait(driver,2).until(EC.presence_of_element_located((By.XPATH,".//*[@id='advanced-view-switch']")))
+                # advancedViewSwitch = driver.find_element_by_xpath(".//*[@id='advanced-view-switch']")
                 advancedViewSwitch.click()
             except NoSuchElementException:
-                log(f'Cant find Advanced View Switch element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Advanced View Switch element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue 
+            log('Advanced View Switch: OK')    
 
             # ------------------------------ Fulfill by Merchant ------------------------------ # 
             try:
                 fbm = driver.find_element_by_xpath(".//input[@type='radio' and @value='MFN']")
                 driver.execute_script("arguments[0].click();", fbm)
-                # time.sleep(1)
-                if (fbm is not None):
-                    break 
             except NoSuchElementException:
-                log(f'Cant find Fulfill by Merchant element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Fulfill by Merchant element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue
-
+            log('Fulfill by Merchant: OK')       
 
             # ------------------------------ Product Condition ------------------------------ #
             _count = 0
@@ -157,15 +164,16 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
                         break
                     # break after 10 times failed
                     if _count >= 10:
-                        log('Cant find the element "New" after 10 times. Skip to next field...', True)
+                        log('Cant find the element "New" after 10 times. Skip to next field...', True, True)
                         break
                     # time.sleep(1)
                 except NoSuchElementException:
-                    log(f'Cant find Product condition element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                    log(f'Cant find Product condition element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                     shouldContinue = True
                     break
             if shouldContinue:
                 continue
+            log('Product Condition: OK') 
 
             # ------------------------------ Product SKU ------------------------------ #     
             try:
@@ -175,8 +183,10 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
                     driver.find_element_by_xpath('//*[@id="item_sku"]').send_keys(line[2])
                 log('Import SKU: OK')
             except NoSuchElementException:
-                log(f'Cant find Contribution SKU element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Contribution SKU element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue        
+            log('Product SKU: OK')  
+
 
             # ------------------------------ Handling Time ------------------------------ # 
             try:
@@ -186,8 +196,10 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
                     driver.find_element_by_xpath('//*[@id="fulfillment_latency"]').send_keys('5')
                 log('Handling Time: OK')
             except NoSuchElementException:
-                log(f'Cant find Handling time element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Handling time element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue
+            log('Handling Time: OK')  
+
 
             # ------------------------------ Product Quantity ------------------------------ # 
             try:
@@ -198,8 +210,10 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
                 
                 log('Quantity: OK')
             except NoSuchElementException:
-                log(f'Cant find Quantity element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Quantity element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue
+            log('Product Quantity: OK')  
+
 
             # ------------------------------ Product Price ------------------------------ #
             try:
@@ -210,27 +224,28 @@ with open(f'{BASE_DIRECTORY}\\Freelancer.csv','r') as csv_file:
                 
                 log('Price: OK')
             except NoSuchElementException:
-                log(f'Cant find Price element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Price element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue
+            log('Product Price: OK')          
 
             # ------------------------------ Save and Finish ------------------------------ #
             try:
                 if newLayout:    
                     driver.find_element(By.XPATH,"//kat-button[@id='EditSaveAction']").click()
+                    # print("saved new layout")
                 else:
                     driver.find_element_by_xpath('//*[@id="EditSaveAction"]').click()
+                    # print("saved old layout")
                 countSuccess += 1
                 log('Save and finish: OK')
-                log(f'Product offer successfuly. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Product offer successfuly. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
             except NoSuchElementException:
-                log(f'Cant find Save and finish element. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
+                log(f'Cant find Save and finish element. {MOVE_TO_NEXT_PRODUCT_STRING}', True, True)
                 continue
 
             time.sleep(1)
         except Exception as e:
-            log(f'Catched exception. Check stack trace below. {MOVE_TO_NEXT_PRODUCT_STRING}', True)
-            f.write(e)
             continue
-    log(f'End session! {countSuccess} / {productCount} / {row_count} (total) products offer successfully!', True)
+    log(f'End session! {countSuccess} / {productCount} / {row_count} (total) products offer successfully!', True, True)
     f.close()
 
